d0bf9104deea8c2accb074ed99ca3216ffc781cb
osuvak <suvako@gmail.com>
2015-10-20 13:33:12 +0300

    norms test - more comments

--- matlab/norms/norms_test.m
+++ matlab/norms/norms_test.m
@@ -12,13 +12,14 @@ c = 4.5;
 rng('shuffle');
 v = c * rand(N,1);
 
-%  compute norms in several ways
+%  compute norms in several ways and put them in a struct array
 norms = [];
 norms(1).val = norm(v);
 norms(2).val = norm(v,2);
 norms(3).val = sqrt(sum(v.*v));
 norms(4).val = sqrt(sum(v.^2));
 
+%  call the nested function to compute errors
 disp('1 and 2')
 disp(relativeError(norms(1).val,norms(2).val))
 disp('2 and 3')
@@ -26,6 +27,7 @@ disp(relativeError(norms(2).val,norms(3).val))
 disp('3 and 4')
 disp(relativeError(norms(3).val,norms(4).val))
 
+%  inner function in nested functions
 function err = relativeError(a,b)
 err = norm(a-b) / (norm(a)+norm(b)/2);
 end

5eba4bd343b7179b63f7b8673a4e66a5327a7099
osuvak <suvako@gmail.com>
2015-10-27 12:01:45 +0200

    bisection method in c with struct llist fpointer initiated

---
+++ c_cpp/nl_eqn_solns/bisection_method/c/multi/struct_llist_fpointer/functions/functions_objective.cc
@@ -0,0 +1,35 @@
+#include "functions_objective.hh"
+
+double
+func_bias_diode
+(
+  double VD
+)
+{
+  double R   = 1e3;
+  double IS  = 3e-15;
+  double VIN = 3.3;
+  double VT  = 25e-3;
+  
+  double m;
+  m = VT * log( ( VIN - VD ) / ( R * IS ) ) - VD;
+  
+  return m;
+}
+
+double
+func_bias_diode_with_exp
+(
+  double VD
+)
+{
+  double R   = 1e3;
+  double IS  = 3e-15;
+  double VIN = 3.3;
+  double VT  = 25e-3;
+  
+  double m;
+  m = VIN - VD - R * IS * exp(VD/VT);
+  
+  return m;
+}
